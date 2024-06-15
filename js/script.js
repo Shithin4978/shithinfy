620bdad23b90cc01e69c43b2333172e4a108682e
shithinvm <shithinbhasker726@gmail.com>
2024-06-15 20:11:42 +0530

    Create script.js

---
+++ js/script.js
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Firebase CRUD Example</title>
+    <script src="https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"></script>
+    <script src="https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"></script>
+</head>
+<body>
+    <h1>Firebase CRUD Operations</h1>
+
+    <input type="text" id="item-name" placeholder="Item Name">
+    <button id="create-btn">Create Item</button>
+
+    <br>
+
+    <ul id="item-list"></ul>
+
+    <script src="script.js"></script>
+</body>
+</html>
